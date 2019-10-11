3d795e72eedacd551bb4f4420b3d3c65f2ce8bdd
Avinash <>
2019-10-11 11:11:56 +0530

    feat(survey_shrikes_web): login and register controller integration with local storage

--- src/app/feature/login/login.component.ts
+++ src/app/feature/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LocalStorageService } from '../dashboard/local-storage.service';
+import { User } from './user';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  email: string;
+  password: string;
+  constructor(private localStorageService: LocalStorageService, private router: Router,
+              private dialogRef: MatDialogRef<LoginComponent>) { }
 
   ngOnInit() {
 
+  }
+
+  login() {
+    const user: User[] = this.localStorageService.getLoginData(this.email, this.password);
+    if (user.length > 0) {
+      localStorage.setItem('loginUser', JSON.stringify(user[0]));
+      this.router.navigate(['dashboardhome/createSurvey']);
+      this.dialogRef.close();
+    } else {
+      alert('Incorrect email or password');
+    }
   }
 
 }
--- src/app/feature/register/register.component.ts
+++ src/app/feature/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../login/user';
+import { LocalStorageService } from '../dashboard/local-storage.service';
+import { Router } from '@angular/router';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  user: User = {} as User;
 
-  constructor() { }
+  constructor(private localStorageService: LocalStorageService,
+              private router: Router,
+              private dialogRef: MatDialogRef<RegisterComponent>) {
+  }
 
   ngOnInit() {
+  }
+
+  register() {
+    this.localStorageService.setLoginData(this.user);
+    localStorage.setItem('loginUser', JSON.stringify(this.user));
+    this.dialogRef.close();
+    this.router.navigate(['dashboardhome/createSurvey']);
   }
 
 }
